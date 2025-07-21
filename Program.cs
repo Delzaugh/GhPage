8eddd32f779aae6061910bc93ae07b99b6d6793b
Jonas Claessens <jonas.claessens@live.com>
2025-07-21 17:31:25 +0200

    Added Mudblazor

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
+using BlazorWebAssemblyXrefGenerator;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorWebAssemblyXrefGenerator;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddHttpClient("APIClient", client => client.BaseAddress = new Uri("https://learn.microsoft.com"));
